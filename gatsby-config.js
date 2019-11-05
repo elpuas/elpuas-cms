bdb6417610c838742fc2231a0f8826b181187b90
Alfredo Navas <elpuas@gmail.com>
2019-11-05 10:59:21 -0600

    dark-mode round one

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,6 +37,13 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-styled-components-dark-mode`,
+      options: {
+        light: require(`${__dirname}/src/theme.js`).lightTheme,
+        dark: require(`${__dirname}/src/theme.js`).darkTheme,
+      },
+    },
+    {
       resolve: `gatsby-plugin-mdx`,
       options: {
         extensions: [".mdx", ".md"],
---
+++ src/theme.js
@@ -0,0 +1,11 @@
+export const darkTheme = {
+    mainColor: 'topaz',
+    secondaryColor: 'ruby',
+}
+
+export const lightTheme = {
+    mainColor: 'pampas',
+    secondaryColor: 'ruby',
+    buttonColor: 'shadyLady',
+    // Note both themes don't necessary have to match (you'll probably want them to though)
+}
