5524d9581ae2502d6d9ea453030ff69ecb3a8261
Alfredo Navas <elpuas@gmail.com>
2019-11-05 11:20:21 -0600

    add netlify forms support

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Button from "../components/button"
 import Instagram from "../components/instagram"
+import Form from "../components/forms"
 
 class IndexPage extends React.Component {
   render() {
@@ -23,11 +24,12 @@ class IndexPage extends React.Component {
             👋
           </span>
         </h1>
-        <Instagram />
         <p>Hi, my name is Alfredo Navas, better known as el.puas and I am a Web Artisan based in the beautiful Costa Rica.</p>
         <p>I run a small workshop of Web Design & Development, I specialize in custom WordPress Development, eCommerce and the JAMStack, speacilly with GatsbyJS.</p>
         <p>I also help my local WordPress community as a organizer/speaker of the MeetUp Group , organizer and speaker of WordCamp San Jose, volunteer and speaker of several camps that include WordCamp US, WordCamp MIA, WordCamp Managua and WordPress Rome Group.</p>
         <p>And more recently the Gatsby Costa Rica Group, making regular meetups with the React Community</p>
+        <Form />
+        <Instagram />
         <Link to="/blog/">
           <Button marginTop="35px">Go to Blog</Button>
         </Link>
--- src/theme.js
+++ src/theme.js
@@ -1,11 +1,11 @@
-export const darkTheme = {
-    mainColor: 'topaz',
-    secondaryColor: 'ruby',
-}
+// export const darkTheme = {
+//     mainColor: 'topaz',
+//     secondaryColor: 'ruby',
+// }
 
-export const lightTheme = {
-    mainColor: 'pampas',
-    secondaryColor: 'ruby',
-    buttonColor: 'shadyLady',
-    // Note both themes don't necessary have to match (you'll probably want them to though)
-}
+// export const lightTheme = {
+//     mainColor: 'pampas',
+//     secondaryColor: 'ruby',
+//     buttonColor: 'shadyLady',
+//     // Note both themes don't necessary have to match (you'll probably want them to though)
+// }
