5a66906cc812aa8a2dab3f282d6fad8cea571a1f
Alfredo Navas <elpuas@gmail.com>
2019-11-05 13:14:26 -0600

    change checkbox

--- src/components/layout.js
+++ src/components/layout.js
@@ -63,7 +63,6 @@ class Layout extends React.Component {
             marginRight: `auto`,
             maxWidth: rhythm(24),
             padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
-            backgroundColor: 'var(--bg)',
             color: 'var(--textNormal)',
             transition: 'color 0.2s ease-out, background 0.2s ease-out',
           }}
